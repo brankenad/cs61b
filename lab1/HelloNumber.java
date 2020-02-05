e667e400307153db9d91103c13f9d2254fdc230b
brankenad <brankenad@example.com>
2020-02-06 03:04:38 +0800

    finish the lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,11 @@
+public class HelloNumber{
+    public static void main(String[] args){
+		int n= 0;
+		int num = 0;
+		while (n < 10){
+	    	num = num + n;
+	    	System.out.print(num + " ");
+	    	n = n + 1;
+		}
+    }
+}
